afaebaf42be1e1fed44cb15fdda020950b7018a8
Nurikov <asankylovnursultan@gmail.com>
2023-10-24 22:40:02 +0600

    Тестирование

--- Очередь/Очередь.cpp
+++ Очередь/Очередь.cpp
@@ -1,10 +1,15 @@
 ﻿#include <iostream>
+#include<Windows.h>
 using namespace std;
 
 
 int main()
 {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
     setlocale(LC_ALL, "ru");
+
+    cout << "Hello World" << endl;
 
     return 0;
 }
